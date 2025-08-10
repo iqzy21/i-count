53df395d2734cf3fff0ce3f4c344b06c5c089a1b
iqzy21 <iqbaal.el21@gmail.com>
2025-08-10 04:18:06 +0100

    K: Added comments to code, updated HTML title

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
-FROM python:3.8-slim
+#using ta lightweight python image as the base  
+FROM python:3.8-slim 
+#selecting the directory for the container to use
 WORKDIR /app
+#taking the dependencies flask and redis within the requirments.txt
+#copy this first so that docker has better caching
 COPY requirements.txt .
+#installing redis and flask dependencies from the requirments.txt
+#install without caching to keep image small
 RUN pip install --no-cache-dir -r requirements.txt
+#copying the rest that is in this directory for the container
 COPY . .
+#setting the port the container should use
 EXPOSE 5003
+#running the flask application 
 CMD [ "python", "app.py" ]
--- app.py
+++ app.py
@@ -12,17 +12,20 @@ redis_password = os.environ.get('REDIS_PASSWORD', None)
 # Redis connection
 r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 
+#specific page routing
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html', title="i-count Home")
 
 @app.route('/count')
 def count():
+    #incrementation of visitor page 
     try:
         visits = r.incr('visit_count')
-        return render_template('count.html', visits=visits)
+        return render_template('count.html', visits=visits, title="i-count Visit Counter")
     except redis.exceptions.ConnectionError:
-        return render_template('count.html', visits='Connection Error')
+        return render_template('count.html', visits='Connection Error', title="Visit Counter")
 
+#host and port establishment
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)
